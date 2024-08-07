ae525ea3bada1ed48e2d80bad45357e59195631a
ioanedurglishvili <durglishviliioane@gmail.com>
2024-08-07 17:05:21 +0400

    (commit)

---
+++ level 25/homework/lisen.py
@@ -0,0 +1,21 @@
+# for i in range(10): 
+#     print("ioane")
+# for i  in range(10):
+#     print("goa")
+# for i in range(10):
+#     print("cow")
+# for i in range(10):
+#     print("python")
+# for i in range(10):
+#     print("html")
+
+# while 5 < 10:
+#     print("ioane")
+# while 5 < 10:
+#     print("goa")
+# while 5 < 10:
+#     print("cow")
+# while 5 < 10:
+#     print("python")
+# while 5 < 10:
+#     print("html")
