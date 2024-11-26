e17f2c7e5ed5896456e03d468963856380d53e0b
ioanedurglishvili <durglishviliioane@gmail.com>
2024-11-26 20:05:50 +0400

    commit

--- day 15/homework/kji.py
+++ day 15/homework/kji.py
@@ -1,4 +1,23 @@
 #i =0
 #while i < 100:
    # print()
-   # i += 2    
+   # i += 2
+#i= int(input("enter a  number:"))
+#if i <0:
+#   print(False)
+#elif i > 0:
+#  print(True) 
+#age= int(input("enter your age"))
+#if age / 4:
+ #  print("შენ ხარ ნაკიანი")
+#else:
+ #     print("შენ არ ხარ ნაკიანი")
+#score= int(input("enter number 1 to 100"))
+# if score >= 90:
+   # print( "Your grade is A")
+# elif score==80 or 90:
+   # ptint( "Your grade is b")
+# elif score==70 or 80:
+#    ptint( "Your grade is c")
+# elif score>=70:
+#    ptint( "Your grade is d")         
