a5a0c7432f48482cdc422d05fa9babffdbcbaa29
ioanedurglishvili <durglishviliioane@gmail.com>
2024-12-07 18:27:27 +0400

    commit

---
+++ day 07/classwork/lisen.py
@@ -0,0 +1,9 @@
+name = input("enter your name ")
+last_name = input("enter your last name ")
+aga = input("enter your aga ")
+country= input("enter your country ")
+color = input(" enter your favorite color ")
+car = input("enter your favorite car ")
+
+little_sentence = ("Heiio! my name is"+name+""+"my last name is"+last_name+"i am from"+country+""+"my favorite color is"+color+"my favorite car is" +car) 
+print(little_sentence)
